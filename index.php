6bf5173be07b4dc36cf5281fdc9ffc7c8e9790fb
iwanzor <iwanzor@gmail.com>
2018-11-27 17:12:50 +0100

    Update

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 // Install the library by downloading the .zip file to your project folder.
 // Load the library
 require_once 'Quiubas.php';
